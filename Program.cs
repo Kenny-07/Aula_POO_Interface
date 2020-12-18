59f4adc4ecd834baea34f65f98e69535b7d860ae
DEV <m.rodriguessilva75@outlook.com>
2020-12-18 15:32:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_POO_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
